7190e82399192c7221628440d53f9f8f244c27af
precious <precious@i.ua>
2012-03-09 02:09:04 +0200

    first and probably last commit

--- xankicard.sh
+++ xankicard.sh
@@ -4,6 +4,6 @@ PYTHON="`which python`"
 SCRIPTS_DIR="/home/precious/programming/python/xankicard"
 DECK="/home/precious/.anki/decks/test_deck.anki"
 
-selection="`xsel`"
+selection="`xsel | sed -r 's/(^\s+)|(\s+$)//g'`"
 translation="`$PYTHON $SCRIPTS_DIR/translate.py \"$selection\" --html`"
 $PYTHON $SCRIPTS_DIR/addcard.py -f "$selection" -b "$translation" $DECK
